60517a2704a9c468030c14d89a9d7eb6bc688c85
jcrnieto <jcrnietos@gmail.com>
2025-07-08 19:29:50 -0300

    configuracion de cors

--- src/server.ts
+++ src/server.ts
@@ -13,10 +13,17 @@ const PORT = 3000;
 
 
 app.use(express.json());
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}))
+// app.use(cors({
+//   origin: 'http://localhost:5173',
+//   credentials: true
+// }))
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 app.use(morgan('dev'));
 app.use('/api/users', userRoutes);
 app.use('/api/personalData', personalDataRoutes);
