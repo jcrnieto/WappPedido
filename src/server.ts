0b7be938491c91ff678a620ae6a59ab6b8c3549a
jcrnieto <jcrnietos@gmail.com>
2025-07-09 17:38:29 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -45,6 +45,13 @@ app.get('/', (req: Request, res: Response) => {
   res.send('¡Hola mundo desde Express!');
 });
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
